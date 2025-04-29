90c63eab01718dc2aaee887637a560684c046bf9
DevMadrine <madrinenamulindwa90@gmail.com>
2025-04-29 23:23:44 +0300

    Added front and backend functionality

--- app/src/main/java/com/hydeinnovations/camera/MainActivity.kt
+++ app/src/main/java/com/hydeinnovations/camera/MainActivity.kt
@@ -6,12 +6,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.camera.core.CameraSelector
 import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -19,7 +20,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.core.app.ActivityCompat
@@ -72,7 +72,18 @@ CameraPreview(
 )
 
                     IconButton(
-                        onClick = {}
+                        onClick = {
+                            controller.cameraSelector = if (
+                                controller.cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA){
+                                CameraSelector.DEFAULT_BACK_CAMERA
+                            }else{
+                              CameraSelector.DEFAULT_FRONT_CAMERA
+                            }
+
+                        },
+
+                        modifier = Modifier
+                            .offset(16.dp, 16.dp)
                     ) {
    Icon(imageVector = ImageVector.vectorResource(R.drawable.ic_user), contentDescription = "")
 
